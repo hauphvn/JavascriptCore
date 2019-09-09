458a5522a04456c0006ce7516faa32623b8be019
Hau <hau.pham@gigacover.com>
2019-09-09 12:38:48 +0700

    Task03's Callback from Coder Tokyo

--- Callback.js
+++ Callback.js
@@ -1,8 +1,8 @@
 //Practicing for myself
-var Sleep = function () {
+function Sleep() {
     console.log("He is sleeping...")
 }
-var Eat = function (sleep) {
+function Eat(sleep) {
     console.log("He is eating...");
     console.log("He have eaten.");
     sleep();
@@ -54,3 +54,28 @@ function sumAndDo(nums, callback) {
 }
 
 sumAndDo([1, 2, 3], double);
+
+console.log("-----------Ex 03 ------------");
+/**
+ * 1. Viết hàm transform nhận vào 1 array gồm các số và 1 function callback. Nội dung của hàm bao gồm:
+ * - Khai báo 1 biến result có giá trị là 1 empty array
+ * - Lặp qua từng phần tử trong array, với mỗi phần tử, gọi hàm callback và truyền vào phần tử đó, được kết quả bao nhiêu thì push vào array khai báo ở trên
+ * - Trả về result array
+ */
+function transform(numbers, callback) {
+    result = [];
+    if (Array.isArray(numbers)){
+        for (x of numbers){
+            result.push(callback(x));
+        }
+    }
+    return result;
+}
+
+function double2(num) {
+    return num * 2;
+}
+
+var output = transform([2, 4, 6], double2);
+console.log(output);
+// Expect: [4, 8, 12]
