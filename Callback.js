8009315683e84ba489d2eb242bc175be440777a5
Hau <hau.pham@gigacover.com>
2019-09-09 12:29:23 +0700

    Task02's Callback from Coder Tokyo

--- Callback.js
+++ Callback.js
@@ -1,14 +1,12 @@
 //Practicing for myself
-Sleep = function () {
+var Sleep = function () {
     console.log("He is sleeping...")
 }
-
-Eat = function (sleep) {
+var Eat = function (sleep) {
     console.log("He is eating...");
     console.log("He have eaten.");
     sleep();
 }
-
 Eat(Sleep);
 console.log("---------Practicing of CoderX-----------");
 //Practicing of CoderX
@@ -31,3 +29,28 @@ function countAndDo(callback) {
 }
 
 countAndDo(sayHello);
+
+console.log("-----------Ex 02 ------------");
+/**
+ * 1. Viết hàm double nhận vào 1 số bất kì, hiển thị ra số đó nhân đôi
+ * 2. Viết hàm sumAndDo nhận vào 1 array và 1 callback function. Hàm sumAndDo làm nhiệm vụ tính tổng array đó, sau đó gọi callback function với tham số là kết quả tổng vừa tính
+ */
+
+function double(num) {
+    if (!isNaN(num)){
+        console.log(num*2);
+    }
+}
+
+function sumAndDo(nums, callback) {
+    sum = 0;
+    if (Array.isArray(nums)){
+        for (x of nums){
+            sum += x;
+        }
+        callback(sum);
+    }
+
+}
+
+sumAndDo([1, 2, 3], double);
