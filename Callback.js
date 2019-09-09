08f2a8a0ba78452ed33797458b807b76a2fb5115
Hau <hau.pham@gigacover.com>
2019-09-09 12:16:18 +0700

    Exercise 01 of Callback from CoderX

--- Callback.js
+++ Callback.js
@@ -1,3 +1,4 @@
+//Practicing for myself
 Sleep = function () {
     console.log("He is sleeping...")
 }
@@ -9,3 +10,24 @@ Eat = function (sleep) {
 }
 
 Eat(Sleep);
+console.log("---------Practicing of CoderX-----------");
+//Practicing of CoderX
+/**
+ * 1. Viết hàm sayHello làm nhiệm vụ log ra màn hình 'I love Coders.Tokyo'
+ * 2. Viết hàm countAndDo nhận vào tham số là 1 hàm. Hàm này log ra màn hình từ 1 đến 10 dùng vòng lặp for. Sau khi log xong, chạy hàm được truyền vào ở tham số
+ * 3. Kiểm tra 2 hàm trên bằng cách:
+ *   countAndDo(sayHello)
+ */
+
+function sayHello() {
+    console.log("I love Coders.Tokyo")
+}
+
+function countAndDo(callback) {
+    for (i = 1;i<11;i++){
+        console.log(i);
+    }
+    callback()
+}
+
+countAndDo(sayHello);
